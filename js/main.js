27227ac3c999d5438eec49cec1fafa55785edb91
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-10-16 16:35:23 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/optandoninorganicchemistry/optandoninorganicchemistry.pdf';
 
 
 let pdfDoc = null,
